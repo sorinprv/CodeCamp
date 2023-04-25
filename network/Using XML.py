b0dd55651d740fe06e6ac34cc90f20090a5d6e8c
sorin pirvu <sorin_pirvu@yahoo.com>
2023-04-25 16:10:15 +0300

    xml

--- network/Using XML.py
+++ network/Using XML.py
@@ -10,3 +10,23 @@ data = '''<person>
 tree = ET.fromstring(data)
 print('Name:', tree.find('name').text)
 print('Attr:', tree.find('email').get('hide'))
+
+input = '''<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Bren</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+    </users>
+</stuff>'''
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+for item in lst:
+    print('Name', item.find('name').text)
+    print('ID', item.find('id').text)
+    print('Attribute', item.get('x'))
