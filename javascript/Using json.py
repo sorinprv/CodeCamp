3e330b7a671e3cba9d02f8c69fe4cae19738c206
sorin pirvu <sorin_pirvu@yahoo.com>
2023-04-25 22:53:54 +0300

    api

--- javascript/Using json.py
+++ javascript/Using json.py
@@ -14,3 +14,20 @@ data = '''
 '''
 info = json.loads(data)
 print(info[1]['name'])
+
+input = '''[
+    { "id" : "001",
+        "x" : "2",
+        "name" : "Quincy"
+    } ,
+    { "id" : "009",
+        "x" : "7",
+        "name" : "Mrugesh"
+    }
+]'''
+info = json.loads(input)
+print('User:', len(info))
+for item in info:
+    print('Name', item['name'])
+    print('ID', item['id'])
+    print('Attribute', item['x'])
---
+++ network/Using urllinks.py
@@ -0,0 +1,21 @@
+import ssl
+import urllib.error
+import urllib.parse
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# url = input('Enter - ')
+url='http://www.dr-chuck.com/page1.htm'
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
